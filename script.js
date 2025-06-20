bda0ae4e033382471b58b26fbb606249f97c4f62
elangavi s <kavi32406@gmail.com>
2025-06-20 15:37:00 +0530

    reinitialized

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 let menu=document.querySelector('#menu')
 let navbar=document.querySelector('.navbar')
-let navA=document.querySelector('.navbar a')
+let navA=document.querySelectorAll('.navbar a')
+let sections =document.querySelectorAll('section')
 
-menu.addEventListener('click',()=>{
-    navbar.classList.toggle('navA')
-})
+window.onscroll =()=>{
+    sections.forEach(sec => {
+        let top=window.screenY
+        let offset=sec.offsetTop-150
+        let height =sec.offsetHeight
+        let id=sec.getAttribute('id')
+
+        if(top >= offset && top < offset+height){
+            navA.forEach(links=>{
+                links.classList.remove('active')
+                document.querySelector('header nav a[herf*='+id+']').classList.add('active')
+            })
+        }
+        
+    });
+}
+menu.onclick=()=>{
+    navbar.classList.toggle('active')
+}
